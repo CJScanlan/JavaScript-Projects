6577b43617380eb5f1b6fafe4dd05b7a0f5a128d
Carly Scanlan <carlyscanlan@gmail.com>
2020-09-29 10:33:43 -0700

    Create new files
    Main.js and corresponding HTML

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+
+
+document.write('Hello,World!');
+
+var A = "This is a string";
+
+document.write(A);
+
+var B ="Cheeto is cute";
+
+window.alert(B);
+
+document.write("Don\'t you like \"eggs\"?");
+
+document.write("Apples" + " and Bananas");
+
+var pets ="dogs and cats", cat = "Charlie", dog = "Mala";
+
+document.write(cat);
+
+var pets = pets.fontcolor("orange"); //code is assigning "orange" to the variable pets
+var cat = cat.fontcolor("purple");
+var dog = dog.fontcolor("brown");
+document.write(dog);
+document.write(cat);
+document.write(pets);
+
+document.write(5*8);
