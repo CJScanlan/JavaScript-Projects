2a223e275d01517061cc674b56716249319af594
Carly Scanlan <carlyscanlan@gmail.com>
2020-09-30 11:33:47 -0700

    Update main.js

--- Basic JavaScript Projects/Project3_math_operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -1,45 +1,45 @@
-function math() {
+function math() { //addition function
     var addition = 2 + 2;
     document.getElementById("math").innerHTML = "2 + 2 = " + addition;
 }
 
-function subtraction() {
+function subtraction() { //subtraction function
     var subtraction = 5 - 2;
     document.getElementById("math2").innerHTML = "5 - 2 = " + subtraction;
 }
 
-function multiply() {
+function multiply() { //multiplication function
      var multiplication = 5 * 27;
      document.getElementById("demo").innerHTML = "5 * 27 = " + multiplication;
 }
 
-function multiple_math() {
+function multiple_math() { //functions with multiple math operations
     var value = 2 * 8 / 16 + 6;
     document.getElementById("demo2").innerHTML = "2 multiplied by 8 then divided by 16 and added by 6 equals " + value;
 }
 
-function modulus_Operation() {
+function modulus_Operation() { //modulus operation to find remainder of division operation
     var simple_math = 25 % 6;
     document.getElementById("math3").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_math;
 }
 
-function negate() {
+function negate() { //creates negative number
     var x = 12;
     document.getElementById("negate").innerHTML = -x;
 }
 
-var z = 13;
+var z = 13; //decrements number by 1 using --
 z--;
 document.write(z);
 
-var y = 15;
+var y = 15; //increments number by 1 using ++
 y++;
 document.write(y);
 
 
-window.alert(Math.random() * 100);
+window.alert(Math.random() * 100); //creates an alert box with a random number between 0 and 100
 
 
-function power() {
+function power() { //power function 
 document.getElementById("power").innerHTML = Math.pow(7,3);
 }
